533bf4aa52f7ed655532b842a2cdbe45d3bd9c24
Mounir Afifi <mounir.afifi@intern.promatis.de>
2020-06-14 18:34:02 +0200

    correcting view

--- fibo/fib/views.py
+++ fibo/fib/views.py
@@ -5,16 +5,17 @@ from fib.models import FibonacciSums
 # Create your views here.
 def fib(request,number):
     number = int(number)
- 
+    F = FibonacciSums(number)
     #res = '<br>'.join([str(x) for x in F.adjust_result(F.get_fibo_sums())])
-    res = FibonacciSums(number).__repr__('<br>',' + ')
-    
+    sums = F.get_fibo_sums()
+    F.adjust_result(sums)
+    res = F.__repr__('<br>',' + ')
+    F.save_to_db()
     return HttpResponse(str(res))
 
 
 def index(request):
     message = "This app is powered by Fibonacci sequences"
-                
     return HttpResponse(message)
 
 
